577e4defcaa677401e491639821aac0ee7255b19
Stephanie Patton <stephaniepatton@stephanies-air.attlocal.net>
2019-04-30 16:45:46 -0500

    completed endpoints for data

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+const zooRouter = require("./zoo-router.js")
+
+server.use('/api/zoo', zooRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Finding the zoo animals..!</h2>
+    `);
+});
 
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+module.exports = server;
---
+++ zoo-router.js
@@ -0,0 +1,77 @@
+const knex = require('knex')
+const express = require('express')
+const router = express.Router();
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lambda.sqlite3'  
+    },
+    useNullAsDefault: true, 
+  }
+  
+  const zooDb = knex(knexConfig)
+  
+  router.get('/', async (req, res) => {
+    try {
+        const zoo = await zooDb('zoos')
+        res.status(200).json(zoo)
+    } catch {
+        res.status(500).json({
+            message: 'Could not find any animals!'
+        })
+  }
+  });
+  
+  router.get('/:id', async (req, res) => {
+   const { id } = req.params
+   try {
+       const getID = await zooDb('zoos')
+       .where({ id: id })
+       .first()
+       getID ? res.status(200).json(getID) : res.status(404).json({message: "Could not find an animal with that ID"})
+   } catch (error) {
+       res.status(500).json({message: 'Something went wrong...'})
+   }
+  });
+  
+  router.post('/', async (req, res) => {
+      const { body, id }  = req
+    try {
+        const newZoo = await zooDb('zoos').insert(body)
+        .where({ id: body[0] })
+        .first()
+       res.status(201).json(newZoo)
+    } catch {
+        res.status(500).json({message: 'Something went wrong...'})
+    }
+  });
+  
+  router.put('/:id', async (req, res) => {
+    const {body} = req
+    const {id} = req.params
+    try {
+        const zooID = await zooDb('zoos')
+        .where({ id: id})
+        .update(body)
+        res.status(200).json(zooID)
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong...'})
+    }
+  });
+  
+  router.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const zooID = await zooDb('zoos')
+        .where( {id: id})
+        .delete()
+        res.status(204).json({message: "Animal safely removed!"})
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong...'})
+    }
+  });
+
+
+
+module.exports = router;
